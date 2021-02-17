1581c5f67e35c5803aed2403d013b572c3888303
mboti2000 <mboti20000@gmail.com>
2021-02-17 13:07:37 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_KEY ='b0eca7a0-fa6d-401d-9341-9d41cddd9b27';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2500;
